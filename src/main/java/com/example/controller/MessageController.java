e0bec78101d32072b41f0eb7ba8c2b00b59056cc
David Green <dcg1695@rit.edu>
2024-09-11 22:14:51 +0000

    implemented repositories and service layers

---
+++ src/main/java/com/example/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.entity.Message;
+import com.example.service.MessageService;
+import com.example.repository.AccountRepository;
+import java.util.List;
+
+@RestController
+public class MessageController {
+    //
+}
---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.entity.Account;
+import com.example.service.AccountService;
+
+@RestController
+public class UserController {
+    //
+}
